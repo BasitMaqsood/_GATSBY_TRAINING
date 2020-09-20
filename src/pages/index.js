11cd7dd086f04f08b1bd4e61c67492445a81ce23
Basit <malikbasitmaqsood@gmail.com>
2020-09-20 21:00:40 +0500

    hero image

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,24 @@ import Layout from '../components/layout';
 import usePosts from '../hooks/use-posts';
 import PostPreview from '../components/post-preview';
 import ReadLink from '../components/read-link';
+import Hero from '../components/hero';
+
 
 export default () => {
   const posts = usePosts();
   return (
-    <Layout>
-      <h1>Home</h1>
-      <p>Hello Basit Maqsood !</p>
-      <ReadLink to="/about/">Learn about me &rarr;</ReadLink>
+    <>
+      <Hero />
+      <Layout>
+        <h1>Home</h1>
+        <p>Hello Basit Maqsood !</p>
+        <ReadLink to="/about/">Learn about me &rarr;</ReadLink>
 
-      <h2>Read my blog</h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post} />
-      ))}
-    </Layout>
+        <h2>Read my blog</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+      </Layout>
+    </>
   );
 };
