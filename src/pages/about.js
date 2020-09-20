8b22729461205c68bd4a2c0a3507a93073dc5251
Basit <malikbasitmaqsood@gmail.com>
2020-09-20 15:38:06 +0500

    gatsby styling with emotions

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 const AboutUS = () => {
     return (
-        <>
+        <Layout>
             <h1>About Us</h1>
             <p>This is my Professional WEbsite</p>
             <Link to="/"> &lrr; back to Home</Link>
-        </>
+        </Layout>
     );
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import React from "react"
 import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
-      <p>Hello Basit Maqsood</p>
-      <Link to="/about">Goto About &rarr;</Link>
+      <Layout>
+        <h1>Home</h1>
+        <p>Hello Basit Maqsood</p>
+        <Link to="/about">Goto About &rarr;</Link>
+      </Layout>
 
     </>
   )
